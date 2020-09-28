2620c4f80f7cae99f3da988fe4dbbffdd4503788
Karol <karolbolejniczak@gmail.com>
2020-09-28 09:16:56 +0200

    added beans

--- src/main/java/pl/karol/shortenerapp/ShortenerappApplication.java
+++ src/main/java/pl/karol/shortenerapp/ShortenerappApplication.java
@@ -2,9 +2,24 @@ package pl.karol.shortenerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class ShortenerappApplication {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate() {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortenerappApplication.class, args);
