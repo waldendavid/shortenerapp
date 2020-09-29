fa96a5c9d70ec5d9bd653a2829ebefe8b4531199
Karol <karolbolejniczak@gmail.com>
2020-09-30 00:06:37 +0200

    Added new method in Controller,

--- src/main/java/pl/karol/shortenerapp/controller/URLController.java
+++ src/main/java/pl/karol/shortenerapp/controller/URLController.java
@@ -1,7 +1,12 @@
 package pl.karol.shortenerapp.controller;
 
+import javax.xml.stream.events.StartDocument;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import pl.karol.shortenerapp.model.UrlBox;
 import pl.karol.shortenerapp.service.UrlServiceImpl;
 
 @RestController
@@ -22,16 +28,25 @@ public class URLController {
 	public URLController(UrlServiceImpl theUrlService) {
 		urlService = theUrlService;
 	}
+	
+	@GetMapping("/")
+	public String start(Model model) {
+		model.addAttribute("longUrl", new String());
+		return "index";
+	}
 
 	@RequestMapping(value = "/link/{id}", method = RequestMethod.GET)
 	public ModelAndView getLongUrl(@PathVariable String id) {
-	    return new ModelAndView("redirect:" + urlService.getLongUrl(id));
+		return new ModelAndView("redirect:" + urlService.getLongUrl(id));
 	}
 
-
-	@PostMapping("/link")  // shorter annotation that GET above
-	public String addUrl(Model model, @RequestBody String longUrl) {
-		return urlService.makeBoxUrl(longUrl);
+	@PostMapping("/add") // shorter annotation that GET above
+	public String addUrl(@ModelAttribute String longUrl, Model model
+	// ,@RequestBody String longUrl
+	) {
+		model.addAttribute("shortUrl", urlService.makeBoxUrl("LongUrl"));
+		// return urlService.makeBoxUrl(longUrl);
+		return "hello";
 
 	}
 }
