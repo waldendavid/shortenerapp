f1a43962226670313ae49238b3aadc5e4403347d
Karol <karolbolejniczak@gmail.com>
2020-10-03 19:04:20 +0200

    edited Controller - return index.html

--- src/main/java/pl/karol/shortenerapp/ShortenerappApplication.java
+++ src/main/java/pl/karol/shortenerapp/ShortenerappApplication.java
@@ -3,6 +3,7 @@ package pl.karol.shortenerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
--- src/main/java/pl/karol/shortenerapp/controller/URLController.java
+++ src/main/java/pl/karol/shortenerapp/controller/URLController.java
@@ -1,25 +1,19 @@
 package pl.karol.shortenerapp.controller;
 
-import javax.xml.stream.events.StartDocument;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import pl.karol.shortenerapp.model.UrlBox;
 import pl.karol.shortenerapp.service.UrlServiceImpl;
 
-@RestController
-@RequestMapping
+@Controller
 public class URLController {
 
 	private UrlServiceImpl urlService;
@@ -30,8 +24,9 @@ public class URLController {
 	}
 	
 	@GetMapping("/")
-	public String start(Model model) {
-		model.addAttribute("longUrl", new String());
+	public String getIndex(Model model) {
+		String longUrlString = new String();
+		model.addAttribute("longUrl", longUrlString);
 		return "index";
 	}
 
